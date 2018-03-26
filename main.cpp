cefc4e2f94307bd1bd99a68e2bd3c03d3ed72779
Tim Hutton <tihutt@microsoft.com>
2018-03-26 19:07:39 +0100

    Now shows an animated colorful triangle.

--- main.cpp
+++ main.cpp
@@ -1,55 +1,45 @@
-#include <exception>
 #include <functional>
 
 #include <emscripten.h>
 #include <SDL.h>
-#include <SDL_image.h>
-#include <SDL_ttf.h>
 
 #define GL_GLEXT_PROTOTYPES 1
 #include <SDL_opengles2.h>
 
 // Shader sources
 const GLchar* vertexSource =
-    "attribute vec4 position;    \n"
-    "void main()                  \n"
-    "{                            \n"
-    "   gl_Position = vec4(position.xyz, 1.0);  \n"
-    "}                            \n";
+    "attribute vec4 position;                     \n"
+    "void main()                                  \n"
+    "{                                            \n"
+    "  gl_Position = vec4(position.xyz, 1.0);     \n"
+    "}                                            \n";
 const GLchar* fragmentSource =
     "precision mediump float;\n"
     "void main()                                  \n"
     "{                                            \n"
-    "  gl_FragColor = vec4 (1.0, 1.0, 1.0, 1.0 );\n"
+    "  gl_FragColor[0] = gl_FragCoord.x/640.0;    \n"
+    "  gl_FragColor[1] = gl_FragCoord.y/480.0;    \n"
+    "  gl_FragColor[2] = 0.5;                     \n"
     "}                                            \n";
 
+// an example of something we will control from the javascript side
+bool background_is_black = true;
+
+// the function called by the javascript code
+extern "C" void EMSCRIPTEN_KEEPALIVE toggle_background_color() { background_is_black = !background_is_black; }
 
 std::function<void()> loop;
 void main_loop() { loop(); }
 
-int main(int argc, char** argv)
+int main()
 {
-    // SDL_Init(SDL_INIT_VIDEO);
-
-    auto wnd(
-        SDL_CreateWindow("test", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
-            640, 480, SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN));
+    SDL_Window *window;
+    SDL_CreateWindowAndRenderer(640, 480, 0, &window, nullptr);
 
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 2);
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0);
-    SDL_GL_SetSwapInterval(0);
     SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
     SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
-
-    auto glc = SDL_GL_CreateContext(wnd);
-
-    auto rdr = SDL_CreateRenderer(
-        wnd, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_TARGETTEXTURE);
-
-    // Create Vertex Array Object
-    //GLuint vao;
-    //glGenVertexArraysOES(1, &vao);
-    //glBindVertexArrayOES(vao);
 
     // Create a Vertex Buffer Object and copy the vertex data to it
     GLuint vbo;
@@ -62,19 +52,18 @@ int main(int argc, char** argv)
 
     // Create and compile the vertex shader
     GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(vertexShader, 1, &vertexSource, NULL);
+    glShaderSource(vertexShader, 1, &vertexSource, nullptr);
     glCompileShader(vertexShader);
 
     // Create and compile the fragment shader
     GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fragmentShader, 1, &fragmentSource, NULL);
+    glShaderSource(fragmentShader, 1, &fragmentSource, nullptr);
     glCompileShader(fragmentShader);
 
     // Link the vertex and fragment shader into a shader program
     GLuint shaderProgram = glCreateProgram();
     glAttachShader(shaderProgram, vertexShader);
     glAttachShader(shaderProgram, fragmentShader);
-    // glBindFragDataLocation(shaderProgram, 0, "outColor");
     glLinkProgram(shaderProgram);
     glUseProgram(shaderProgram);
 
@@ -85,24 +74,26 @@ int main(int argc, char** argv)
 
     loop = [&]
     {
-        SDL_Event e;
-        while(SDL_PollEvent(&e))
-        {
-            if(e.type == SDL_QUIT) std::terminate();
-        }
+        // move a vertex
+        const uint32_t milliseconds_since_start = SDL_GetTicks();
+        const uint32_t milliseconds_per_loop = 3000;
+        vertices[0] = ( milliseconds_since_start % milliseconds_per_loop ) / float(milliseconds_per_loop) - 0.5f;
+        glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-
-        // Clear the screen to black
-        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+        // Clear the screen
+        if( background_is_black )
+            glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+        else
+            glClearColor(0.9f, 0.9f, 0.9f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
         // Draw a triangle from the 3 vertices
         glDrawArrays(GL_TRIANGLES, 0, 3);
 
-        SDL_GL_SwapWindow(wnd);
+        SDL_GL_SwapWindow(window);
     };
 
     emscripten_set_main_loop(main_loop, 0, true);
 
-    return 0;
+    return EXIT_SUCCESS;
 }
--- readme.md
+++ readme.md
@@ -15,9 +15,21 @@ Instructions:
     
 3. Build index.js and index.wasm:
 
-    ```emcc main.cpp -std=c++11 -s WASM=1 -s USE_SDL=2 -s USE_SDL_IMAGE=2 -s USE_SDL_TTF=2 -O3 -o index.js```
+    ```emcc main.cpp -std=c++11 -s WASM=1 -s USE_SDL=2 -O3 -o index.js```
 
-4. Open index.html in a web browser. You should see ...
+4. Open index.html in a web browser. You should see a colorful animated triangle:
+
+    <img width="400px" src="https://user-images.githubusercontent.com/647092/37932094-866c158e-313f-11e8-84f9-3873223373c5.png" />
+
+    Chrome doesn't support file:// XHR requests, so you need to first start a webserver, e.g.:
+
+    with Python 2: ```python -m SimpleHTTPServer 8080```
+    
+    with Python 3: ```python -m http.server 8080```
+
+    and then open this URL:
+
+    http://localhost:8080/
 
 
 MIT license:
